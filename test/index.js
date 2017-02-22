7ba550ad6d62254624f3488c311db4f941d77639
Nicola Pedretti <pedrettin@guild.dhcp.wustl.edu>
2017-02-22 12:36:09 -0600

    removing the default keys and secret -- they are not used anymore so no need to delete them from git history

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const should = require('should')
 describe('jwplayer index', function () {
 
   it('should print an error if api_key and api_secret are not specified', done => {
-      const jwplayer = require('../src/index.js')({api_key: 'TCZkoYFj', api_secret: 'qISqUKnbFiy6GwMuimuN3W8g'})
+      const jwplayer = require('../src/index.js')({api_key: 'XXXX', api_secret: 'XXXXX'})
       done()
   })
 
